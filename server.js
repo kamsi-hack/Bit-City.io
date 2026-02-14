8a951a4029b9634a2d32b088c580518232e66180
kamsi-hack <kamsinwabueze155@gmail.com>
2026-02-14 18:53:28 +0100

    Initialize WebSocket server for order book

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const WebSocket = require("ws");
+const app = express();
+
+const server = app.listen(5000);
+const wss = new WebSocket.Server({ server });
+
+let orderBook = { bids: [], asks: [] };
+
+wss.on("connection", ws => {
+    ws.send(JSON.stringify(orderBook));
+
+    ws.on("message", message => {
+        const order = JSON.parse(message);
+
+        if(order.type === "buy")
+            orderBook.bids.push(order);
+        else
+            orderBook.asks.push(order);
+
+        broadcast();
+    });
+});
+
+function broadcast() {
+    wss.clients.forEach(client =>
+        client.send(JSON.stringify(orderBook))
+    );
+}
